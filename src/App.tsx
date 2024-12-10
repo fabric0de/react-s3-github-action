ea5f9bcb7ff4a8670a8bfe36e14251b2d8cdf479
김정현 (Kim JungHyeon) <junghyeonkim.dev@gmail.com>
2024-12-10 13:47:10 +0900

    test: Github Action 테스트를 위한 커밋

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css'
 
 function App() {
-
-  return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button className='btn btn-secondary'>Helol</button>
-    </>
-  )
+	return (
+		<>
+			<h1 className='text-3xl font-bold underline'>Test Github Action</h1>
+			<div className='flex items-center justify-between'>
+				<button className='btn btn-secondary'>취소</button>
+				<button className='btn btn-secondary'>확인</button>
+			</div>
+		</>
+	)
 }
 
 export default App
